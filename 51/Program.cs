c888ea1ae932596b9df425b08b09c482d0caaa07
Taleh Mirzayev <40539143+taboy@users.noreply.github.com>
2022-02-27 23:14:19 +0300

    Update Program.cs

--- 51/Program.cs
+++ 51/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Задача 51: Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+ int[,] massiv=new int[5,5];
+void fillArray(int[,]filled){
+    for(int i=0;i<filled.GetLength(0);i++){
+        for(int j=0;j<filled.GetLength(1);j++){
+            filled[i,j]=i+j;
+        }
+    }
+}
+void printArray(int[,]printed){
+    for(int i=0;i<printed.GetLength(0);i++){
+        for(int j=0;j<printed.GetLength(1);j++){
+            Console.Write($"{printed[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+fillArray(massiv);
+printArray(massiv);
 
